f8306d74b90344ad29dccfcc5ca597e4897961b3
ivemotta <ive_motta@hotmail.com>
2019-09-19 12:12:59 -0300

    código python

---
+++ classe.py
@@ -0,0 +1,69 @@
+# receita = input("o que você quer cozinhar?")
+# pedir_ingredientes = input("do que você vai precisar?")
+# pedir_modo_de_fazer = input("como fazer a receita?")
+# questao = input("sua receita é doce?")
+# class Receita():
+#     def __init__(self, ingredientes, modo_de_fazer):
+#         self.ingredientes = ingredientes
+#         self.modo_de_fazer = modo_de_fazer
+#     def get_ingredientes(self):
+#         print("Para fazer {}, você vai precisar de {}.".format(receita, pedir_ingredientes))
+#     def get_modo_de_fazer(self):
+#         print ("Para fazer {}, você precisa {}".format(receita, pedir_modo_de_fazer))
+#     def set_ingredientes(self, novos_ingredientes):
+#         novos_ingredientes = input("qual nova receita gostaria de fazer?")
+#         self.ingredientes = novos_ingredientes
+#         print("para fazer {}, execute novamente o terminal".format(novos_ingredientes))
+#     def set_modo_de_fazer(self, pedir_modo_de_fazer):
+#         self.modo_de_fazer = pedir_modo_de_fazer
+
+# resultado = Receita (receita, pedir_ingredientes)
+# resultado.get_ingredientes()
+# resultado.get_modo_de_fazer()
+# #resultado.set_ingredientes(pedir_ingredientes)
+
+# class Doce(Receita):
+#     def __init__(self, ingres, modo):
+#         self.ingres = ingres
+#         self.modo = modo
+#     def get_ingres (self):
+#         print ("para fazer a calda de {}, voce precisará de {}".format(receita, ingres))
+#     def get_modo (self):
+#         print ("então a calda se faz assim: {} {}".format(modo, ingres))
+
+# class Salgado(Receita):
+#             def __init__(self, fritadeira):
+#                 self.fritadeira = fritadeira
+#             def get_fritadeira(self):
+#                 print ("adicione oleo na fritadeira e frite seu{}". format(receita))
+
+# if questao == "sim":
+#     calda = input("gostaria de adicionar calda?")
+#     if calda == "sim":
+#         ingres = input ("quais ingres queres?")
+#         modo = input ("como faz essa calda?")
+#         docinho = Doce(calda, modo)
+#         docinho.get_ingres()
+#         docinho.get_modo()
+#     else:
+#         print("aff então ta")
+# else:
+#     fritar = input ("quer fritar sua receita?")
+#     if fritar == "sim":
+#         fritadeira = input("você tem fritadeira?")
+#         if fritadeira == "sim":
+#             salgadinho = Salgado(fritadeira)
+#             salgadinho.get_fritadeira()
+#         else:
+#             print ("então compra otario")
+#     else:
+#         print ("hum saudavel")
+
+
+
+
+
+for i in ingredientes(input("qual ingrediente?"))
+    ingredientes = int(input("quantos ingredientes voce quer?"))
+    lista = ingredientes
+print (lista)
